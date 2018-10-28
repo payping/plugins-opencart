cbde06772ad6d87968dcf0029d635e005cfc9a05
Erfan Ebrahimi <persionhost@gmail.com>
2018-10-29 00:01:10 +0330

    Create payping.php

---
+++ admin/language/fa-ir/extension/payment/payping.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']       = '<b style="color:#1E91CF;">پی‌پینگ</b>';
+
+// Text 
+$_['text_extension']   	  = 'افزونه ها';
+$_['text_success']        = 'ماژول پرداخت پی‌پینگ با موفقیت ویرایش شد!';
+$_['text_payping']  	  = '<a href="https://www.payping.ir/" target="_blank"><img src="view/image/payment/payping.png" alt="پی‌پینگ" title="پی‌پینگ" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_edit']           = 'ویرایش پرداخت آنلاین پی‌پینگ';
+$_['text_support']        = 'پشتیبانی';
+$_['text_support_title']  = 'عرفان ابراهیمی';
+
+// Entry
+$_['entry_pin']   		  = 'توکن پی‌پینگ';
+$_['entry_order_status']  = 'وضعیت سفارش';
+$_['entry_status']        = 'وضعیت';
+$_['entry_sort_order']    = 'ترتیب';
+
+// Error
+$_['error_permission']    = 'هشدار شما اجازه ویرایش ماژول پی‌پینگ را ندارید!';
+$_['error_pin']   		  = 'توکن پی‌پینگ مورد نیاز است!';
