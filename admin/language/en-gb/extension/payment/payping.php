90bc5fe5d828d0c60ef84818154f39844ea0beed
Erfan Ebrahimi <persionhost@gmail.com>
2018-10-31 18:14:26 +0330

    Create payping.php

---
+++ admin/language/en-gb/extension/payment/payping.php
@@ -0,0 +1,19 @@
+
+<?php
+// Heading
+$_['heading_title']       = '<b style="color:#1E91CF;">PayPing</b>';
+// Text 
+$_['text_extension']	  = 'Extensions';
+$_['text_success']        = 'Success : Modificaions was successfully done!';
+$_['text_payping']  	  = '<a href="https://www.payping.ir/" target="_blank"><img src="view/image/payment/payping.png" alt="payping" title="payping" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_edit']           = 'Edit payping Online Payment';
+$_['text_support']        = 'Support Forum';
+$_['text_support_title']  = 'erfan ebrahimi';
+// Entry
+$_['entry_pin']   		  = 'PayPing TOKEN';
+$_['entry_order_status']  = 'Order Status';
+$_['entry_status']        = 'Status';
+$_['entry_sort_order']    = 'Sort Order';
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify payment payping!';
+$_['error_pin']   		  = 'TOKEN is required!';
